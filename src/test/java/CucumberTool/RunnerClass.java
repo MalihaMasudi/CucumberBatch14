2a2209e66641ef00cd96972e7f99773c5d39465f
MalihaMasudi <hzazmo@gmail.com>
2022-12-22 07:17:03 -0500

    commit the message

---
+++ src/test/java/CucumberTool/RunnerClass.java
@@ -0,0 +1,21 @@
+package CucumberTool;
+
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features =
+                "C:\\Users\\maliha\\CucumberBatch14\\src\\test\\java\\CucumberTool\\Login.feature"
+)
+
+
+public class RunnerClass {
+}
+
+
+
+
